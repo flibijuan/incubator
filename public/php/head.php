44d9768c3c540a79c78bd53a69178968ba3ddc3c
adam <adam@kiron.university>
2015-09-28 01:03:04 +0200

    first add of website to public

---
+++ public/php/head.php
@@ -0,0 +1,25 @@
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+<?php include_once($docpath . "/css/themify-icons.css"); ?>
+</style>
+
+<style>
+<?php include_once($docpath . "/css/bootstrap.min.css"); ?>
+</style>
+
+<style>
+<?php include_once($docpath . "/css/flexslider.css"); ?>
+</style>
+
+<style>
+<?php include_once($docpath . "/css/et-line-icons.css"); ?>
+</style>
+
+<style>
+<?php include_once($docpath . "/css/theme.css"); ?>
+</style>
+
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
---
+++ public/php/timeline.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html >
+	    <link rel="stylesheet" href="css/timeline.css">
+
+
+	<div class="container">
+    <h3 class="project-name" style="margin-bottom:80px">Your Path at Kiron Ventures</h3>
+		<div id="timeline">
+			<div class="timeline-item">
+				<div class="timeline-content">
+					<h2>1) Get selected</h2>
+					<p>Apply, pitch and get selected to take part in the programme.</p>
+					</div>
+			</div>
+
+			<div class="timeline-item">
+				<div class="timeline-content right">
+				<h2>2) Benefit from the programme</h2>
+	            <p>Get financial support, office space, technical support, professional advice and access to a broad network (Kiron, MigrationHub and others) during a period of three months.</p>
+				</div>
+			</div>
+
+			<div class="timeline-item">
+				<div class="timeline-content">
+				<h2>3) START-UP BOAT</h2>
+				<p>Get an invite to the Start-Up boat, an event to create personal connections with the community and investors.</p>
+				</div>
+			</div>
+
+
+            <div class="timeline-item">
+                <div class="timeline-content right">
+                    <h2>4) Mentor and investor</h2>
+                    <p>At the end of the program if your idea proves to have a market fit, sustainability and more importantly a social impact, an investor will support you with a small capital investment and a senior start-up will be appointed as your mentor.</p>
+                </div>
+            </div>
+
+    <div class="timeline-item">
+				<div class="timeline-content">
+					<h2>5) Fundraising</h2>
+		               <p>Together with our partners we will continue to support you and advise you on how to apply for various financing sources.</p>
+						</div>
+			</div>
+
+
+
+		</div>
+	</div>
+    
+
+    
+    
+    
+  </body>
+</html>
