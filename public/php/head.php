5d6e1005cc183b90839b84a6cb66a29a05844e3d
adam <adam@kiron.university>
2015-09-30 13:01:24 +0200

    adding favicons

--- public/php/head.php
+++ public/php/head.php
@@ -1,5 +1,12 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="icon" type="image/png" href="/img/favicon/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/img/favicon/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/img/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
+<link rel="shortcut icon" href="/img/favicon/favicon.ico">
+
 <style> <?php include_once($docpath . "/css/themify-icons.min.css"); ?> </style>
 <style> <?php include_once($docpath . "/css/bootstrap.min.css"); ?></style>
 <style> <?php include_once($docpath . "/css/flexslider.css"); ?> </style>
@@ -7,5 +14,6 @@
 <style> <?php include_once($docpath . "/css/theme.min.css"); ?> </style>
 <style> <?php include_once($docpath . "/css/timeline.css"); ?> </style>
 <link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
+
 <style> <?php include_once($docpath . "/css/classic-081711.css"); ?> </style>
 <style type='text/css'> #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; } </style>
