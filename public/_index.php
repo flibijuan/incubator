f29f0725e515a99361d3cabab2dbe1ec5d0e40ae
adam <adam@kiron.university>
2015-10-01 23:25:18 +0200

    coming soon page

---
+++ public/_index.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang=en>
+<head>
+<title>Kiron Ventures | Home </title>
+<?php $docpath = $_SERVER['DOCUMENT_ROOT'];?>
+<?php include_once($docpath . "/php/head.php"); ?>
+<?php include_once("php/googleanalytics.php") ?>
+</head>
+<body class=btn-rounded>
+<div class=nav-container><nav>
+<?php include_once("php/navbar.php") ?>
+</nav>
+</div>
+<div class=main-container>
+<section class="cover fullscreen image-slider slider-all-controls controls-inside parallax">
+<ul class=slides>
+<li class="overlay image-bg">
+<div class=background-image-holder>
+<img alt=image class=background-image src=img/home22.jpg width=1440 height=960>
+</div>
+<div class="container v-align-transform">
+<div class=row>
+<div class="col-sm-8 col-md-7">
+<h1 class="mb40 mb-xs-16 large">Innovation to create social impact</h1>
+<h6 class="uppercase mb16">Berlin based start-up incubator program, funding driven and innovative entrepreneurs that want to create social impact&nbsp;</h6>
+<p class="lead mb40">Do you have ideas to help tackle the migration crisis?</p>
+<a class="btn btn-lg" href=#>Pitch us</a>
+</div>
+</div>
+</div>
+</li><li class="overlay image-bg">
+<div class=background-image-holder>
+<img alt=image class=background-image src=img/cover8.jpg width=1280 height=853>
+</div>
+<div class="container v-align-transform">
+<div class=row>
+<div class="col-sm-8 col-md-11">
+<h1 class="mb40 mb-xs-16 large">6.5 million refugees worldwide.&nbsp;<br>Let's give them a future.</h1>
+<h6 class="uppercase mb16">Ventures to support refugees </h6>
+<p class="lead mb40">Apply today for financial support, office space, technical support, professional advice and access to a broad network</p>
+<a class="btn btn-lg" href=#>Pitch us</a>
+</div>
+</div>
+</div>
+</li>
+</ul>
+</section><section>
+<div class=container>
+<div class="row mb64 mb-xs-24">
+<div class="col-sm-12 col-md-10 col-md-offset-1 text-center">
+<h3>Applications are open!<br>Several new social impact projects will be launched soon</h3>
+</div>
+</div>
+<div class=row>
+<div class="text-center col-sm-3">
+<div class=feature>
+<div class=text-center>
+<i class="mb40 mb-xs-24 inline-block color-primary et-line-megaphone icon-lg"></i>
+<h5 class=uppercase>OPEN CALL &nbsp;<br></h5>
+</div>
+<p>Apply now to be part of the Kiron Ventures Impact Program with a refugee focus (we are looking for seed and pre-seed stage ideas).&nbsp;<br></p>
+</div>
+</div>
+<div class="text-center col-sm-3">
+<div class=feature>
+<div class=text-center>
+<i class="mb40 mb-xs-24 inline-block color-primary et-line-beaker icon-lg"></i>
+<h5 class=uppercase>FIRST ROUND<br>pitch day</h5>
+</div>
+<p>A selected group of startups will pitch to us and have personal feed back on their idea.
+</p>
+</div>
+</div><div class="text-center col-sm-3">
+<div class=feature>
+<div class=text-center>
+<i class="mb40 mb-xs-24 inline-block color-primary et-line-trophy icon-lg"></i>
+<h5 class=uppercase>final selection to take part in the program</h5>
+</div>
+<p>At the end of every month, after having assessed the projects' viability, sustainability and social impact, Kiron will announce the selected startups.
+</p>
+</div>
+</div><div class="text-center col-sm-3">
+<div class=feature>
+<div class=text-center>
+<i class="mb40 mb-xs-24 inline-block color-primary et-line-linegraph icon-lg"></i>
+<h5 class=uppercase>KICK START<br>3 MONTHS PROGRAM&nbsp;</h5>
+</div>
+<p>In partnership with Social Impact Lab, the entrepreneurs will benefit from our advice, network, technologies and office spaces.</p>
+</div>
+</div>
+</div>
+</div>
+</section><section class="page-title page-title-2 image-bg overlay parallax">
+<div class=background-image-holder>
+<img alt="Background Image" class=background-image src=img/cover15.jpg width=1440 height=960>
+</div>
+<div class=container>
+<div class=row>
+<div class=col-md-8>
+<h2 class="uppercase mb8">Migrationhub</h2>
+<p class="lead mb0"><span>Migration hub is an open physical space for ideas and partnerships to blossom.&nbsp;<br></span>You can share ideas, learn about other entrepreneurs' solutions and maybe meet cofounders for your next impact project.<br><br><b>Address: Berlin (Potsdamer Strasse 144, 10783 Berlin).</b><br><br>The opening hours are extensive, but naturally not unlimited. Come take a look until December 2015!</p>
+</div>
+</div>
+</div>
+</section><section>
+<div class=container>
+<div class=row>
+<?php include_once("php/timeline.php"); ?>
+</div>
+</div>
+</section><section class="image-bg bg-dark parallax overlay pt120 pb120">
+<div class=background-image-holder>
+<img alt="Background Image" class=background-image src=img/boat.jpg width=1280 height=800>
+</div>
+<div class=container>
+<div class=row>
+<div class="col-sm-12 text-center">
+<h2 class="uppercase mb8">The start-up boat</h2>
+<p class="lead mb40">Selected entrepreneurs will have the opportunity to participate on the start-up boat.<br>An event&nbsp;
+designed for the integration of corporates, investors and start-ups to help to tackle social problems through the design thinking methodology.</p>
+<a class="btn btn-lg btn-white mb0" href=http://startupboat.eu/ target=_blank>learn more</a>
+</div>
+</div>
+</div>
+</section><section class=bg-secondary>
+<div class=container>
+<div class=row>
+<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center">
+<h2>Our partners</h2>
+<p class="mb40 mb-xs-24">with our partners we want to give the environment and network to create impact</p>
+</div>
+</div>
+<div class=row>
+<div class="col-sm-12 text-center spread-children-large">
+<div class="col-sm-2 text-center"> <img class=img-responsive alt=Pic src=img/UNCHRB.png width=612 height=612></div>
+<div class="col-sm-2 text-center"> <img class=img-responsive alt=Pic src=img/SILB.png width=612 height=612></div>
+<div class="col-sm-2 text-center"> <img class=img-responsive alt=Pic src=img/LULB.png width=612 height=612></div>
+<div class="col-sm-2 text-center"> <img class=img-responsive alt=Pic src=img/RWTHB.png width=612 height=612></div>
+<div class="col-sm-2 text-center"> <img class=img-responsive alt=Pic src=img/HPIB.png width=612 height=612></div>
+</div>
+</div>
+</section><section class="bg-dark pt64 pb64">
+<div class=container>
+<div class=row>
+<div class="col-sm-12 text-center">
+<h2 class=mb8>Where to find us</h2>
+<p class="lead mb40 mb-xs-24">If you want to get in contact with us, get to know about our investment system<br> or have questions about the application process send us an email at<br><br> contact@kironventures.com</p>
+</div>
+</div>
+</div>
+</section><section class="image-bg parallax overlay">
+<div class=background-image-holder>
+<img alt=image class=background-image src=img/home-2-1.jpg width=1600 height=1067>
+</div>
+<div class=container>
+<div class="row mb40 mb-xs-24">
+<div class="col-sm-12 text-center">
+<h3 class=mb8>Subscribe for a monthly roundup of best Kiron ventures start-ups.</h3>
+<p>Don't worry, we hate spam too - that's why we only send out monthly emails.</p>
+</div>
+</div>
+<div class="row mb80 mb-xs-24">
+<div class="col-sm-6 col-sm-offset-3">
+<form class="form-newsletter halves" data-success="Thanks for your registration, we will be in touch shortly." data-error="Please fill all fields correctly.">
+<input type=text name=email class="mb0 transparent validate-email validate-required signup-email-field" placeholder="Email Address">
+<input type=submit class=mb0 value=Subscribe>
+<iframe srcdoc="<div id=&quot;mc_embed_signup&quot;><form action=&quot;//mrare.us8.list-manage.com/subscribe/post?u=77142ece814d3cff52058a51f&amp;id=94d040322a&quot; method=&quot;post&quot; id=&quot;mc-embedded-subscribe-form&quot; name=&quot;mc-embedded-subscribe-form&quot; class=&quot;validate&quot; target=&quot;_blank&quot; novalidate>    <div id=&quot;mc_embed_signup_scroll&quot;>    <h2>Subscribe to our mailing list</h2><div class=&quot;indicates-required&quot;><span class=&quot;asterisk&quot;>*</span> indicates required</div><div class=&quot;mc-field-group&quot;>    <label for=&quot;mce-EMAIL&quot;>Email Address  <span class=&quot;asterisk&quot;>*</span></label>    <input type=&quot;email&quot; value=&quot;&quot; name=&quot;EMAIL&quot; class=&quot;required email&quot; id=&quot;mce-EMAIL&quot;></div><div class=&quot;mc-field-group&quot;>    <label for=&quot;mce-FNAME&quot;>First Name </label>    <input type=&quot;text&quot; value=&quot;&quot; name=&quot;FNAME&quot; class=&quot;&quot; id=&quot;mce-FNAME&quot;></div><div class=&quot;mc-field-group&quot;>    <label for=&quot;mce-LNAME&quot;>Last Name </label>    <input type=&quot;text&quot; value=&quot;&quot; name=&quot;LNAME&quot; class=&quot;&quot; id=&quot;mce-LNAME&quot;></div>    <div id=&quot;mce-responses&quot; class=&quot;clear&quot;>        <div class=&quot;response&quot; id=&quot;mce-error-response&quot; style=&quot;display:none&quot;></div>        <div class=&quot;response&quot; id=&quot;mce-success-response&quot; style=&quot;display:none&quot;></div>    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->    <div style=&quot;position: absolute; left: -5000px;&quot;><input type=&quot;text&quot; name=&quot;b_77142ece814d3cff52058a51f_94d040322a&quot; tabindex=&quot;-1&quot; value=&quot;&quot;></div>    <div class=&quot;clear&quot;><input type=&quot;submit&quot; value=&quot;Subscribe&quot; name=&quot;subscribe&quot; id=&quot;mc-embedded-subscribe&quot; class=&quot;button&quot;></div>    </div></form></div><script type='text/javascript' src='js/mc-validate.min.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->" class="mail-list-form">
+</iframe>
+</form>
+</div>
+</div>
+</div>
+</section><footer class="footer-2 bg-dark text-center-xs">
+<div class=container>
+<div class=row>
+<div class=col-sm-4>
+<a href=#><div class=vnu><img class="image-xxs fade-half" alt=Pic src=img/kironventureslogo4.png></div></a>
+</div>
+<div class="col-sm-4 text-center">
+<span class=fade-half>
+© Copyright 2015 kiron ventures - All Rights Reserved &nbsp;contact us at contact@kironventures.com<br>
+</span>
+</div>
+<div class="col-sm-4 text-right text-center-xs">
+<ul class="list-inline social-list">
+<li><a href=#><i class=ti-facebook></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</footer></div>
+<script src=/js/head.load.min.js></script>
+<script>head.load("/js/jquery.min.js","/js/bootstrap.min.js","/js/flexslider.min.js","/js/parallax.js","/js/scripts.min.js");</script>
+</body>
+</html>
