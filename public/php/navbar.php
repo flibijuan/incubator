9383dcf066ae7912151d9b37bbfb39982a1ecb26
adam <adam@kiron.university>
2015-10-06 23:11:22 +0200

    shortening file names for url display

--- public/php/navbar.php
+++ public/php/navbar.php
@@ -6,15 +6,15 @@
 </div>
 <div class="col-sm-12 col-md-5 col-md-pull-2 overflow-hidden-xs">
 <ul class="menu inline-block pull-right">
-<li><a href="/kiron-ventures-investors.min.php">investors&nbsp;</a></li>
+<li><a href="/investors.min.php">investors&nbsp;</a></li>
 <li><a href="#">corporate&nbsp;</a></li>
-<li><a href="/kironapply.min.php">Apply&nbsp;</a></li>
+<li><a href="/apply.min.php">Apply&nbsp;</a></li>
 </ul>
 </div>
 <div class="col-sm-12 col-md-5 pb-xs-24">
 <ul class="menu">
-<li><a href="/about-incubator.min.php">About&nbsp;</a></li>
-<li><a href="/kironvolunteers.min.php">Volunteers&nbsp;</a></li>
+<li><a href="/about.min.php">About&nbsp;</a></li>
+<li><a href="/volunteers.min.php">Volunteers&nbsp;</a></li>
 <li><a href="#">comunity&nbsp;</a></li>
 </ul>
 </div>
