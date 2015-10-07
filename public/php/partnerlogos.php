830c3c654cd7ec95e2899461f05fcbb21c12af56
adam <adam@kiron.university>
2015-10-07 14:56:37 +0200

    update images

---
+++ public/php/partnerlogos.php
@@ -0,0 +1,30 @@
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/180.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/ankomer2.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/make.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/UNCHRB.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/HPIB.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/SILB.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/RWTHB.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/SB.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/MH.png">
+</div>
+<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+<img class="img-responsive" alt="Pic" src="img/SAP.png">
+</div>
