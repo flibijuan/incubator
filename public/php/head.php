b61cf599c6f7b795bc05e5f9aa103ec57ca1b2e1
adam <adam@kiron.university>
2015-10-12 11:09:06 +0200

    add google, facebook metadata

--- public/php/head.php
+++ public/php/head.php
@@ -7,6 +7,18 @@
 <link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
 <link rel="shortcut icon" href="/img/favicon/favicon.ico">
 
+<meta name="author" content="Adam Roe"/>
+<meta name="keywords" content="startup,incubator,investment,refugee,help,found,founder"/>
+<meta name="description" content="Berlin based Startup incubator. Funding driven, innovative entrepreneurs creating solutions for the refugee crisis"/>
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<meta property="og:site_name"content="Kiron Ventures" />
+<meta property="og:url" content="http://kironventures.com">
+<meta property="og:type" content="website">
+<meta property="og:title"content="Funding innovative entrepreneurs creating solutions for the refugee crisis" />
+<meta property="og:image"content="http://analogphotographyberlin.com/img/home22-fb.jpg" />
+<meta property="og:description"content="Kiron ventures is a Berlin based start-up incubator program, funding driven and innovative entrepreneurs that want to create social impact, by creating solutions for the refugee crisis.">
+
 <style> <?php include_once($docpath . "/css/themify-icons.min.css"); ?> </style>
 <style> <?php include_once($docpath . "/css/bootstrap.min.css"); ?></style>
 <style> <?php include_once($docpath . "/css/flexslider.css"); ?> </style>
